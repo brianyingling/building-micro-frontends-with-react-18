84069c75ecb040120179cb122894bb03c2d89c28
brianyingling <brian_yingling@yahoo.com>
2023-12-30 22:48:17 -0500

    committed

--- ch7/ebuy/libs/mocks/src/lib/product-list-mocks.tsx
+++ ch7/ebuy/libs/mocks/src/lib/product-list-mocks.tsx
@@ -25,6 +25,12 @@ export const PRODUCT_LIST_MOCKS: Array<productListItem> = [
     title: 'Bananas',
     image: '/assets/bananas.jpg',
     price: 0.7,
+  },
+  {
+    id: '5',
+    title: 'grapes',
+    image: '/assets/bananas.jpg',
+    price: 0.7,
   }
 ];
 
